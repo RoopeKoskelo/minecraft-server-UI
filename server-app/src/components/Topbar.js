90ff3c0ea09406c885996e2c56a234a285b4e745
RoopeKoskelo <roope.koskelo@gmail.com>
2023-03-08 22:57:48 +0200

    header size

--- server-app/src/components/Topbar.js
+++ server-app/src/components/Topbar.js
@@ -22,7 +22,7 @@ export default function ButtonAppBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
             Server Panel
           </Typography>
           <Button color="inherit">Login</Button>
