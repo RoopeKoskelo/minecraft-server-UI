1f035ea133b64ff9ea3eedd17981590196744756
RoopeKoskelo <roope.koskelo@gmail.com>
2023-03-13 11:12:23 +0200

    VITUT ei se toimi ei

--- server-app/src/components/StartButton.js
+++ server-app/src/components/StartButton.js
@@ -3,24 +3,9 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 export default function StartButton() {
-    const { spawn } = require('node:child_process');
-
-    let [state, setState] = useState(false);
-
-    function startServer(){
-        spawn('cmd.exe', ['/c', 'C:\Users\Oppilas1\Desktop\VS kamat\MineservuPOGOY\server-files\start.bat']);
-    }
-
-    useEffect(() => {
-        if (state = true){
-            startServer()
-        return
-        }
-    });
-
     return(
     <Box display='flex'>
-        <Button variant='contained' size= "large" color="secondary" onClick={() => setState(true)}>
+        <Button variant='contained' size= "large" color="secondary">
             Start Server
         </Button>
     </Box>
