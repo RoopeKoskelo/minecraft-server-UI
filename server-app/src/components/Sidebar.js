159f541f163b7c29ba31b8838efc378c5387d44f
FinC121 <kaltiainensisu@gmail.com>
2023-03-28 08:10:56 +0000

    Sidebar worker

--- server-app/src/components/Sidebar.js
+++ server-app/src/components/Sidebar.js
@@ -12,11 +12,22 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import {ThemeProvider} from '@mui/material';
 import theme from'../theme'
+import styled from '@emotion/styled';
 
 const drawerWidth = 240;
+//Ei voi käyttää muuttujana FooterToolbaarissa, joten hardcoded siel
+
+const FooterToolbar = styled(Toolbar)`
+  background-color: #813E00;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 240px;
+  text-align: center;
+`;
+
 
 export default function Sidebar() {
   return (
@@ -47,21 +58,30 @@ export default function Sidebar() {
           anchor="left"
         >
           <Toolbar>
-            <h1>POGOY</h1>
+            <Typography variant='h1'>POGOY</Typography>
           </Toolbar>
           <Divider />
           <List>
-            {['Server', 'Options', 'Console', '---'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
+              <ListItem disablePadding>
+                <ListItemButton to="/">
                   <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    <InboxIcon/><ListItemText>Server Home</ListItemText>
                   </ListItemIcon>
-                  <ListItemText primary={text} />
+                  <ListItemText/>
                 </ListItemButton>
               </ListItem>
-            ))}
+              <ListItem disablePadding>
+                <ListItemButton to="/Settings">
+                  <ListItemIcon>
+                    <InboxIcon/><ListItemText>Server Settings</ListItemText>
+                  </ListItemIcon>
+                  <ListItemText/>
+                </ListItemButton>
+              </ListItem>
           </List>
+          <FooterToolbar>
+            <Typography variant='h1'>POGOY</Typography>
+          </FooterToolbar>
         </Drawer>
       </Box>
     </ThemeProvider>
