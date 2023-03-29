7a7239c8771607535f0c8b080850c00c1637343b
RoopeKoskelo <roope.koskelo@gmail.com>
2023-03-29 23:58:40 +0300

    console window working

--- server-app/src/pages/Console.js
+++ server-app/src/pages/Console.js
@@ -1,40 +1,47 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, useRef } from 'react';
 import { Grid, Box } from '@mui/material';
 
 export default function Home() {
     const [messages, setMessages] = useState([]);
+    const messagesEndRef = useRef(null);
 
     function loadData() {
         fetch("http://localhost:8000/logs")
         .then((res) => res.json())
-        .then((data) => setMessages(data.messages));
+        .then((data) => setMessages(data.messages))
+        .catch(err => console.log(err));
     }
 
     useEffect(() => {
         setInterval(() => {
             loadData();
           }, 1000);
+        messagesEndRef.current.scrollIntoView();
     });
 
     return (
         <div>
-            <Grid className="App">
-                <Grid>
-                    <Box sx={{
-                        display: 'flex',
-                        height: 500,
-                        justifyContent: 'space-evenly',
-                        alignItems: 'center',
-                        ml: '240px'
-                    }}>
-                        {
-                            messages.map((item) => {
-                                return <p>{item}</p>
-                            })
-                        }
-                    </Box>
-                </Grid>
-            </Grid>
+            <Box sx={{
+                padding: '65px',
+            }}>
+                <Box sx={{
+                    flex: 1,
+                    padding: '45px',
+                    ml: '240px',
+                    maxHeight: '800px',
+                    bgcolor: '#020202',
+                    color: '#FFFFFF',
+                    borderRadius: 4,
+                    overflowY: 'scroll',
+                }}>
+                    {
+                        messages.map((item) => {
+                            return <p>{item}</p>
+                        })
+                    }
+                    <div ref={messagesEndRef} />
+                </Box>
+            </Box>
         </div>
     );
 }
